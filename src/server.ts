1c6b1a0b28fcdc9ed1c3c35c43b9c6070588210c
Brayden Langley <brayden167@gmail.com>
2025-02-19 13:41:35 -0800

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@
 import app from "./app";
 import { migrateLatest } from "./db/knex";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 async function startServer() {
     try {
