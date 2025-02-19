c539eb9fa43cea2cb2a5717dfe25b778e476c701
Brayden Langley <brayden167@gmail.com>
2025-02-18 16:27:36 -0800

    !

--- scripts/mkenv.sh
+++ scripts/mkenv.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-# mkenv.sh
 
 # Memory limit for the container
 MEMORY_LIMIT="4Gi" # Example: 512Mi, 1Gi, etc.
 
 echo "Creating $1"
-cat <<EOF > $1
-apiVersion: serving.knative.dev/v1
+echo "apiVersion: serving.knative.dev/v1
 kind: Service
 metadata:
-  name: ${SERVICE}
+  name: $SERVICE
   labels:
     cloud.googleapis.com/location: us-west1
 spec:
@@ -17,40 +15,22 @@ spec:
     spec:
       timeoutSeconds: 3540
       containers:
-      - image: ${IMAGE}
+      - image: $IMAGE
         ports:
         - name: h2c
           containerPort: 8080
         resources:
           limits:
-            memory: ${MEMORY_LIMIT}
-        env:
-EOF
+            memory: $MEMORY_LIMIT
+        env:" > $1
 
 echo "Appending environment variables to $1"
 
-# Append environment variables with proper indentation (10 spaces for list items)
-for var in \
-  DB_CONNECTION_NAME \
-  DB_USER \
-  DB_PASS \
-  DB_NAME \
-  TWILIO_ACCOUNT_SID \
-  TWILIO_AUTH_TOKEN \
-  TWILIO_VERIFY_SERVICE_SID \
-  BSV_NETWORK \
-  KNEX_DB_CONNECTION \
-  SERVER_PRIVATE_KEY \
-  TAAL_API_KEY \
-  COMMISSION_FEE \
-  COMMISSION_PUBLIC_KEY \
-  FEE_MODEL; do
-    value=$(printenv "$var")
-    if [ -n "$value" ]; then
-      echo "          - name: $var" >> $1
-      echo "            value: '$value'" >> $1
-    fi
-done
+echo "Appending to $1"
+perl -E'
+  say "        - name: $_
+          value: \x27$ENV{$_}\x27" for @ARGV;
+' DB_CONNECTION_NAME DB_USER DB_PASS DB_NAME TWILIO_ACCOUNT_SID TWILIO_AUTH_TOKEN TWILIO_VERIFY_SERVICE_SID BSV_NETWORK KNEX_DB_CONNECTION SERVER_PRIVATE_KEY TAAL_API_KEY COMMISSION_FEE COMMISSION_PUBLIC_KEY FEE_MODEL >> $1
 
 echo "Built! Contents of $1:"
 cat $1
